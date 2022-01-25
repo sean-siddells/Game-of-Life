7321802b177ef0ce1693da53b8fa73221e6e6788
sean <seansiddells@gmail.com>
2022-01-26 12:34:04 +1300

    nextBoard outputting correct board

--- nextBoard.js
+++ nextBoard.js
@@ -1,23 +1,25 @@
 const nextCellState = require('./nextCellState')
 const countAliveNeighbours = require('./countAliveNeighbours')
 const createBoard = require('./createBoard')
-const getNeighbours = require('./getNeighbours')
 
-const board = createBoard (5)
+const board = createBoard(5)
 
 function nextBoard (currentBoard) {
   let newBoard = []
   for (let i = 0; i < currentBoard.length; i++) {
     newBoard[i] = currentBoard[i].slice()
+    for (let j = 0; j < currentBoard.length; j++) {
+      newBoard[i][j] = currentBoard[i][j]
+    }
   }
   currentBoard.map((row, rowIndex) =>
     row.map((cell, cellIndex) => {
       const aliveNeighbours = countAliveNeighbours(cellIndex, rowIndex, currentBoard)
-      newBoard[rowIndex][cellIndex] = nextCellState(cell, aliveNeighbours)
+      newBoard[cellIndex][rowIndex] = nextCellState(cell, aliveNeighbours)
     })
   )
-  console.log('this is newBoard: ', newBoard)
   console.log('this is currentBoard: ', currentBoard)
+  console.log('this is newBoard: ', newBoard)
   return newBoard
 }
 
